4e99031065702597fb1a3a704da2c20ebe6b36be
gowrishsbhandary <gowrishsbhandary@gmail.com>
2021-05-06 00:54:14 +0300

    First commit

---
+++ src/main/java/com/projectx/spring/hands/on/SpringHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.projectx.spring.hands.on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHandsOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectx/spring/hands/on/SpringHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectx.spring.hands.on;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
