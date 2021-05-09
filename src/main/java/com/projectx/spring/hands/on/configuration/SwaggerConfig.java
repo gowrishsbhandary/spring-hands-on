11254b21c66c8ccb26972f9dbc33de7e3c99eb8c
gowrishsbhandary <gowrishsbhandary@gmail.com>
2021-05-09 21:45:33 +0300

    Overriding apiInfo

--- src/main/java/com/projectx/spring/hands/on/configuration/SwaggerConfig.java
+++ src/main/java/com/projectx/spring/hands/on/configuration/SwaggerConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +20,20 @@ public class SwaggerConfig {
         .select()
         .apis(RequestHandlerSelectors.basePackage("com.projectx.spring.hands.on.controller"))
         .paths(PathSelectors.regex("/user.*"))
-        .build();
+        .build()
+        .apiInfo(metaInfo());
+  }
+
+  private ApiInfo metaInfo() {
+
+    return new ApiInfo(
+        "User Registration",
+        "Register a user to the system",
+        "1.0",
+        "https://projectx/terms",
+        new Contact("Project-X", "https://projectx", "projectx@gmail.com"),
+        "copy rights reserved @ Project-X",
+        "https://projectx/license",
+        new ArrayList<>());
   }
 }
