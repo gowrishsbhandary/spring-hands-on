50b9c981bc2c08e617232887cf1577809ab8a075
gowrishsbhandary <gowrishsbhandary@gmail.com>
2021-05-09 22:29:34 +0300

    Class and field level documentation

--- src/main/java/com/projectx/spring/hands/on/model/User.java
+++ src/main/java/com/projectx/spring/hands/on/model/User.java
@@ -1,5 +1,7 @@
 package com.projectx.spring.hands.on.model;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.persistence.Entity;
@@ -9,13 +11,18 @@ import javax.persistence.Id;
 
 @Data
 @Entity
+@Api(value = "user details")
 public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @ApiModelProperty(value = "Default generated ID")
   private Long id;
 
+  @ApiModelProperty(value = "Name of the user")
   private String name;
+  @ApiModelProperty(value = "Designation of the user")
   private String designation;
+  @ApiModelProperty(value = "Salary of the user")
   private Double salary;
 }
